8e8de9346cf7d66dfb79dfe21c93a7b7b8b39cb7
Mariusz Roliński <mariusz@mondoreale.pl>
2023-06-07 12:29:17 +0200

    Clean-up

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 ;(async function app() {
     const n = 161
 
-    const FPS = 24
+    const FPS = 20
 
     const canvas = document.getElementById('movieClip')
 
     const ctx = canvas.getContext('2d')
 
-    const images = await Promise.all(
-        [...Array(n)].map(
-            (_, i) =>
-                new Promise((resolve, reject) => {
-                    const image = new Image()
+    function getImage(index) {
+        return new Promise((resolve, reject) => {
+            const image = new Image()
 
-                    image.addEventListener('load', () => void resolve(image))
+            image.addEventListener('load', () => void resolve(image))
 
-                    image.addEventListener('error', () => void reject())
+            image.addEventListener('error', () => void reject())
 
-                    image.src = `./CompedFrames/2023 Rail Header Image-v3Comped-Frame-${`000${i}`.slice(
-                        -3
-                    )}.png`
-                })
-        )
-    )
+            image.src = `./frames/2023 Rail Header Image-v3Comped-Frame-${`000${index}`.slice(
+                -3
+            )}.png`
+        })
+    }
 
     let startedAt = undefined
 
     let lastFrameNo = undefined
 
-    const w = images[0].width
-
-    const h = images[0].height
-
     const cw = canvas.width
 
     const ch = canvas.height
+
+    const firstImage = await getImage(0)
+
+    ctx.clearRect(0, 0, cw, ch)
+
+    ctx.drawImage(firstImage, 0, 0, cw, ch)
+
+    const images = await Promise.all([...Array(n)].map((_, i) => getImage(i)))
 
     function onTick(time) {
         if (startedAt == null) {
